0c5e981bfc46ec7d22e31b1f6eab8da5ba81051c
dklug <duncanklug@outlook.com>
2018-04-08 17:19:39 -0700

    Now properly lists objects in the database

--- app/services/AWS-S3.ts
+++ app/services/AWS-S3.ts
@@ -1,11 +1,17 @@
 import { Injectable } from "@angular/core";
 import * as fs from "tns-core-modules/file-system";
 require('nativescript-nodeify');
-import * as AWS from "aws-sdk";
+//import * as AWS from "aws-sdk";
 //import S3 = require('aws-sdk/clients/s3');
 
-//import AWS = require('aws-sdk');
+import AWS = require('aws-sdk');
 //AWS.config.loadFromPath('./aws-credentials.json');
+
+AWS.config.update({
+    accessKeyId: "replaceme",
+    secretAccessKey: "replaceme"
+});
+console.log('aws config updated');
 
 var s3 = new AWS.S3();
 var params = {
@@ -19,27 +25,21 @@ export class aws{
     constructor()
     {
         console.log('aws constructor called\n');
-        AWS.config.update({
-            accessKeyId: "replacethis",
-            secretAccessKey: "replacethis",
-            region: "us-west-2"
-        });
-        console.log('aws config updated');
     }
     downloadAll()
     {
         console.log("downloadAll()");
 
-        s3.listBuckets(function(err, data) {
-            console.log("listBuckets");
-            if (err) console.log(err, err.stack); // an error occurred
-            else     console.log(data);           // successful response
-        });
-
-        // s3.listObjects(params, function(err, data) {
-        //     console.log("listObjects");
+        // s3.listBuckets(function(err, data) {
+        //     console.log("listBuckets");
         //     if (err) console.log(err, err.stack); // an error occurred
-        //     else     console.log(data);           // successful response
+        //     else     console.log(JSON.stringify(data));           // successful response
         // });
+
+        s3.listObjects(params, function(err, data) {
+            console.log("listObjects");
+            if (err) console.log(err, err.stack); // an error occurred
+            else     console.log(JSON.stringify(data));           // successful response
+        });
     }
 }
