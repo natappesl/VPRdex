b6ac86c41ad3d8a1a35a6c17b34b13b513c196fe
dklug <duncanklug@outlook.com>
2018-04-09 18:41:49 -0700

    Now downloads and writes JSON files to local storage

--- app/services/AWS-S3.ts
+++ app/services/AWS-S3.ts
@@ -14,36 +14,111 @@ AWS.config.update({
     accessKeyId: aKI,
     secretAccessKey: sAK
 });
-console.log('aws config updated');
+console.log('AWS config updated');
 
 var s3 = new AWS.S3();
 
 var params = {
   Bucket: 'natappdata',
-  Prefix: 'json/'
+  //Prefix: 'json/'
 };
+
+var documents = fs.knownFolders.documents();
+// console.log("documents:" +JSON.stringify(documents));
+// console.log("documents path:" +documents.path);
+
+var dataPath = fs.path.join(documents.path, "app/data/")
+var dataFolder = fs.Folder.fromPath(dataPath);
+// var exists = fs.File.exists(JSONpath);
+// console.log("exists: " + exists);
+var dataPath_json = fs.path.join(dataPath, "json");
+var dataFolder_json = fs.Folder.fromPath(dataPath_json);
+var dataPath_files = fs.path.join(dataPath, "files");
+var dataFolder_files = fs.Folder.fromPath(dataPath_files);
+
+var currentData = {};
+currentData["json/"] =true;
+currentData["files/"] =true;
+
+dataFolder_json.getEntities()
+    .then(function (entities) {
+        // entities is array with the document's files and folders.
+        entities.forEach(function (entity) {
+            //console.log(entity.name);
+            currentData["json/"+entity.name] = entity;
+        });
+    }, function (error) {
+        // Failed to obtain folder's contents.
+        console.log("fs error" + error.toString());
+    });
+
+dataFolder_files.getEntities()
+    .then(function (entities) {
+        entities.forEach(function (entity) {
+            //console.log(entity.name);
+            currentData["files/"+entity.name] = entity;
+        });
+    }, function (error) {
+        console.log("fs error" + error.toString());
+    });
 
 @Injectable ()
 export class aws{
     public infoURL : string = "http://natappdata.s3.amazonaws.com/";
-    constructor()
-    {
-        console.log('aws constructor called\n');
+    constructor(){
+        //console.log('aws constructor called\n');
     }
-    downloadAll()
-    {
+
+    downloadAll(){
         console.log("downloadAll()");
 
-        // s3.listBuckets(function(err, data) {
-        //     console.log("listBuckets");
-        //     if (err) console.log(err, err.stack); // an error occurred
-        //     else     console.log(JSON.stringify(data));           // successful response
-        // });
+        console.log("Printing local data: ");
+        for (var v in currentData)
+        {
+            console.log(v.toString());
+        }
 
         s3.listObjects(params, function(err, data) {
             console.log("listObjects");
-            if (err) console.log(err, err.stack); // an error occurred
-            else     console.log(JSON.stringify(data));           // successful response
+            if (err) console.log(err.toString(), err.stack.toString()); // an error occurred
+            else{
+                for (var i in data.Contents){
+                    //console.log(data.Contents[i].Key);
+                    if(currentData[data.Contents[i].Key]){
+                        console.log("File already exists!")
+                    }
+                    else{
+                        console.log(data.Contents[i].Key + " does not exist, writing...")
+                        
+                        var getParams = {
+                            Bucket: "natappdata",
+                            Key: data.Contents[i].Key
+                        };
+                        
+                        s3.getObject(getParams, function(err, getData) {
+                            if (err) console.log(err.toString(), err.stack.toString()); // an error occurred
+                            else{
+                                if (getData.ContentType == "application/json")
+                                {
+                                    //console.log((getData.Body));           // successful response
+                                    var writeFile = dataFolder.getFile(data.Contents[i].Key);
+                                    //Writing text to the file.
+                                    writeFile.writeText(JSON.stringify(getData.Body))
+                                    .then(function () {
+                                        // Succeeded writing to the file.
+                                        console.log("File write success")
+                                    }, function (error) {
+                                        // Failed to write to the file.
+                                        console.log("File write error: ");
+                                        console.log(error.toString());
+                                    });
+                                }
+                            }
+                        });
+                    }
+                }
+                //console.log(JSON.stringify(data));           // successful response/
+            }     
         });
     }
 }
